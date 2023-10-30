cb271be5f22610bda9d278161d61d0a4d5b6419c
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-29 20:32:49 -0500

    Create class 'generatorPassword'

---
+++ PasswordGenerator.java
@@ -0,0 +1,9 @@
+
+public class PasswordGenerator {
+
+ public String generatePassword(int i, CharacterRule specialCharRule, CharacterRule lowerCaseRule,
+   CharacterRule upperCaseRule, CharacterRule digitRule) {
+  return null;
+ }
+
+}
