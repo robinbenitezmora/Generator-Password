ba593fe99dc589fb9fb840a4cb6a9a850d6c92bf
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-29 20:33:14 -0500

    Create 'EnglishCharacterData.java" file

---
+++ EnglishCharacterData.java
@@ -0,0 +1,9 @@
+
+public class EnglishCharacterData {
+
+ public static final String LowerCase = null;
+ public static final String UpperCase = null;
+ public static final String Digit = null;
+ public static final String Special = null;
+
+}
